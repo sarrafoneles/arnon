e8f9b8b67ae236e1f0eca86dce3a539e123ec56b
sarrafoneles <50277701+sarrafoneles@users.noreply.github.com>
2019-05-15 21:55:08 -0400

    Add files via upload

---
+++ js/dummydata.js
@@ -0,0 +1,20 @@
+// grid dummy data
+var dummyData = {
+	"Módulo 1":"<br><br><br><!--<P class=\"content-wrap\">Abrange temas de Embriologia Animal e Humana, Histologia e Anatomia e Fisiologia Humana, além da Microbiologia e Parasitologia.</P>--><br><div class=\"foodicon\">Esta seção estará disponível em breve</div>",
+    
+    "Módulo 2":"<br><br><br><!--<P class=\"content-wrap\">Abrange temas de Embriologia Animal e Humana, Histologia e Anatomia e Fisiologia Humana, além da Microbiologia e Parasitologia.</P>--><br><div class=\"foodicon\">Esta seção estará disponível em breve</div>",
+    
+    "Módulo 3":"<br><br><br><!--<P class=\"content-wrap\">Abrange temas de Embriologia Animal e Humana, Histologia e Anatomia e Fisiologia Humana, além da Microbiologia e Parasitologia.</P>--><br><div class=\"foodicon\">Esta seção estará disponível em breve</div>",
+    
+    "Módulo 4":"<br><br><br><!--<P class=\"content-wrap\">Abrange temas de Embriologia Animal e Humana, Histologia e Anatomia e Fisiologia Humana, além da Microbiologia e Parasitologia.</P>--><br><div class=\"foodicon\">Esta seção estará disponível em breve</div>",
+
+	"Módulo 1&nbsp;":"<br><br><br><a href=\"pdf/MOVIMENTO UNIFORME.pdf\" target=\"_blank\"><li class=\"product\", \"grid\"><div class=\"foodicon\">Movimento Uniforme</div></li></a> <a href=\"pdf/MOVIMENTO VARIADO.pdf\" target=\"_blank\"><li class=\"product\", \"grid\"><div class=\"foodicon\">Movimento Variado</div></li></a> ",
+    
+    "Todos":"<br><br><br><a href=\"pdf/MOVIMENTO UNIFORME.pdf\" target=\"_blank\"><li class=\"product\", \"grid\"><div class=\"foodicon\">Movimento Uniforme</div></li></a> <a href=\"pdf/MOVIMENTO VARIADO.pdf\" target=\"_blank\"><li class=\"product\", \"grid\"><div class=\"foodicon\">Movimento Variado</div></li></a> ",
+    
+
+	"Módulo 1&nbsp;&nbsp;":"<a href=\"pdf/Bactéria.pdf\" target=\"_blank\"><li class=\"product\", \"grid\"><div class=\"foodicon\">Conjuntos Numéricos</div></li></a>"+
+							"<a href=\"pdf/Bactéria.pdf\" target=\"_blank\"><li class=\"product\", \"grid\"><div class=\"foodicon\">Conjuntos Numéricos</div></li></a>",
+
+	"Módulo 1&nbsp;&nbsp;&nbsp;":"<a href=\"pdf/Bactéria.pdf\" target=\"_blank\"><li class=\"product\", \"grid\"><div class=\"foodicon\" >Introdução à Química Orgânica</div></li></a>"
+}
